a8477f1aad864cc61a337432690bfd2ff25306e9
Vincent T <vtam2012@gmail.com>
2024-02-21 21:24:25 -0800

    added .gitignore file

--- server/index.js
+++ server/index.js
@@ -1,6 +1,38 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 5000
+const cors = require('cors')
+
+// Middleware
+app.use(cors())
+app.use(express.json())
+
+// MongoDB Config
+const { MongoClient, ServerApiVersion } = require('mongodb');
+require('dotenv').config();
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(process.env.URI, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
